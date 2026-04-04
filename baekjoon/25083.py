6827825d8431b0b3d8cbb8e3aded176b0e8ccdbb
Hyunjin Baek <hyunjin3399@gmail.com>
2026-04-04 22:55:23 +0900

    solve BOJ 25083

---
+++ baekjoon/25083.py
@@ -0,0 +1,10 @@
+# Problem: 25083
+# Tier: Bronz
+
+# \ 는 이스케이프 문자라서 \\ 이렇게 두 번 써야 출력됨
+print("         ,r'\"7")
+print("r`-_   ,'  ,/")
+print(" \\. \". L_r'")
+print("   `~\\/")
+print("      |")
+print("      |")
