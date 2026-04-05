c5d724137768e3c203a100a5295b575727d7fe85
Hyunjin Baek <hyunjin3399@gmail.com>
2026-04-06 06:55:53 +0900

    filter smaller

--- baekjoon/10871.py
+++ baekjoon/10871.py
@@ -1,13 +1,13 @@
 # problem: 10871
 # tier: bronze
 
-# N과 X 입력
+# N, X 입력
 N, X = map(int, input().split())
 
-# 정수 리스트 입력
+# 수열 입력
 arr = list(map(int, input().split()))
 
-# X보다 작은 수 출력
+# X보다 작은 수만 출력
 for i in arr:
     if i < X:
         print(i, end=" ")
