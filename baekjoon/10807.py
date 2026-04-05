ae33d962c987307eac3bf0dc929c1f71a60b1611
Hyunjin Baek <hyunjin3399@gmail.com>
2026-04-06 02:11:04 +0900

    count integer occurrences

--- baekjoon/10807.py
+++ baekjoon/10807.py
@@ -1,14 +1,14 @@
 # problem: 10807
 # tier: bronze
 
-# 정수 개수 입력
+# 정수의 개수 N 입력
 N = int(input())
 
-# 정수 리스트 입력
+# 공백으로 구분된 정수들을 리스트로 저장
 arr = list(map(int, input().split()))
 
-# 찾을 값 입력
+# 개수를 찾으려는 정수 V 입력
 V = int(input())
 
-# 리스트에서 V의 개수 출력
+# 리스트 내 정수 V의 출현 횟수 계산 및 출력
 print(arr.count(V))
