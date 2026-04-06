6e9a3ae0799bc0b6ef88ba5daba988049fc28b5f
Hyunjin Baek <hyunjin3399@gmail.com>
2026-04-06 23:10:24 +0900

    sum three numbers

--- baekjoon/10998.py
+++ baekjoon/10998.py
@@ -1,5 +1,8 @@
 # problem: 10998
 # tier: bronze
 
+# 두 정수 입력 및 변환
 A, B = map(int, input().split())
+
+# 곱셈 결과 출력
 print(A * B)
--- baekjoon/11382.py
+++ baekjoon/11382.py
@@ -1,5 +1,8 @@
 # problem: 11382
 # tier: bronze
 
+# 세 정수 입력 및 변환
 A, B, C = map(int, input().split())
+
+# 세 수의 합 출력
 print(A + B + C)
