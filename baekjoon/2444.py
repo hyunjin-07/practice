65dfe28d29f479e25a337324e89d317b3d6d7a87
Hyunjin Baek <hyunjin3399@gmail.com>
2026-04-17 20:10:05 +0900

    diamond star pattern

--- baekjoon/2444.py
+++ baekjoon/2444.py
@@ -1,18 +1,15 @@
-# Problem: 2444
-# Tier: Bronz
+# problem: 2444
+# tier: bronze
 
-n = int(input())  # 줄 개수 입력
+# 기준 값(n) 입력
+n = int(input())
 
-# 위쪽 피라미드
+# 위쪽 피라미드 (1층부터 n층까지)
 for i in range(1, n + 1):
-    # 공백: (n - i)개
-    print(" " * (n - i), end="")
-    # 별: (2*i - 1)개
-    print("*" * (2 * i - 1))
+    # 공백: (n - i)개, 별: (2*i - 1)개
+    print(" " * (n - i) + "*" * (2 * i - 1))
 
-# 아래쪽 역피라미드
+# 아래쪽 역피라미드 (n-1층부터 1층까지)
 for i in range(n - 1, 0, -1):
-    # 공백: (n - i)개
-    print(" " * (n - i), end="")
-    # 별: (2*i - 1)개
-    print("*" * (2 * i - 1))
+    # 공백: (n - i)개, 별: (2*i - 1)개
+    print(" " * (n - i) + "*" * (2 * i - 1))
