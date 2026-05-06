1588451594ff88f830cd3fbe1de896d247371843
Hyunjin Baek <hyunjin3399@gmail.com>
2026-05-06 20:35:41 +0900

    time complexity 2

---
+++ baekjoon/24263.py
@@ -0,0 +1,14 @@
+# problem: 24263
+# tier: bronze
+
+# MenOfPassion(A, n) {
+#     sum <- 0;
+#     for i from 1 to n
+#         sum <- sum + A[i]; # 코드1
+#     return sum;
+# }
+
+# n번 반복되므로 수행 횟수는 n
+print(input())
+# n의 차수는 1 (선형 시간 복잡도 O(n))
+print(1)
